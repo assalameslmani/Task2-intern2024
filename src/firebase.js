8383b6a21b95ca0360a355ba1b2facd7cb02596b
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2024-08-01 15:05:52 +0300

    add dotenv for more secure in firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyATp3D_mrrThCApSCneLucwl9jWIOyC0jk",
-    authDomain: "lmsproject-433ed.firebaseapp.com",
-    projectId: "lmsproject-433ed",
-    storageBucket: "lmsproject-433ed.appspot.com",
-    messagingSenderId: "981868989331",
-    appId: "1:981868989331:web:10edda74968f109e7ab666",
-    measurementId: "G-890YV85J81"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
